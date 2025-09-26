1a840008ba08336ded7c29b925b3e3982d43d668
traveltime07-hash <traveltime07@gmail.com>
2025-09-26 18:55:29 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,14 @@
+// pages/_app.js
+import Head from "next/head";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
